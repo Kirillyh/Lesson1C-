4491accaf7befbcc8edb56e102f2223ddb7ce46d
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-11-29 07:14:46 +0300

    Добавили Exampel002

---
+++ Exampel002_UserName/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите ваше имя"); // вывод в консоль
+string username = Console.ReadLine();  //ReadLine - считывает строку из терминала. string - строка username - переменная
+Console.WriteLine("Привет,");  //Writeline- выводим в консоль
+Console.WriteLine("username"); // выводим в консоль . Если надо вывести в одной строке используется write
