8405e8d281177af54bfe0dc1899a8195df737be0
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-11-29 08:47:00 +0300

    Добавили Example004_Delen

---
+++ Exampel004_Delen/Program.cs
@@ -0,0 +1,3 @@
+﻿int numberA = 10;                       // Чтобы делить дробные числа dooble  numberA = 10;
+int numbreB = 5;                                                    //dooble  numbreB = 5;
+Console.WriteLine(numberA / numbreB);                               //Console.WriteLine(numberA / numbreB);
