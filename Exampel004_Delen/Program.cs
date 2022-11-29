dca74a5023b998c50e971b3d71eb79e1c8006263
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-11-29 09:04:14 +0300

    Добавили Example004_Delen

--- Exampel004_Delen/Program.cs
+++ Exampel004_Delen/Program.cs
@@ -1,3 +1,11 @@
-﻿int numberA = 10;                       // Чтобы делить дробные числа dooble  numberA = 10;
-int numbreB = 5;                                                    //dooble  numbreB = 5;
-Console.WriteLine(numberA / numbreB);                               //Console.WriteLine(numberA / numbreB);
+﻿//int numberA = 10;                       // Чтобы делить дробные числа dooble  numberA = 10;
+//int numbreB = 5;                                                    //dooble  numbreB = 5;
+//Console.WriteLine(numberA / numbreB);                               //Console.WriteLine(numberA / numbreB);
+
+
+int numberA = new Random() .Next(1, 10);  // случайное число от 1 до 9 Next - целочисленное число int
+Console.WriteLine(numberA);               // Вывод первое число 
+int numbreB = new Random() .Next(1, 10);
+Console.WriteLine(numbreB);               // Вывод второе число
+int resolt = numberA + numbreB;
+Console.WriteLine(resolt);
