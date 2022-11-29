a336eede993ddcca26f401108ff5faa3eb35cf7f
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-11-29 09:43:12 +0300

    Добавили Exampl005_if

---
+++ Exampel005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");  //Ввести имя пользователя в одной строке write
+string username = Console.ReadLine();        //string - строка. username - переменная Console.Readline()-с читываем с консоли
+
+if (username.ToLower() == "Маша")                      // Условие uzerneme == "Маша"   ToLower()-все значения "Маша" будут в нижнем регистре "маша"
+{
+    Console.WriteLine("Ура это же Маша");    // Выводим в консоль
+}
+else                                         // Иначе 
+{
+    Console.Write("Привет, ");               // Выводим в консоль в одной строке
+    Console.WriteLine(username);           // Выводим в консоль по строчно
+}
