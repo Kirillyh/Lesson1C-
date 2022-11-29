074483fb3fa43ee941497316b417a46e35990fbf
Kirillyh <118924718+Kirillyh@users.noreply.github.com>
2022-11-29 07:53:18 +0300

    Добавили Exampel003_sumCisel

---
+++ Exampel003_SumCisel/Program.cs
@@ -0,0 +1,5 @@
+﻿int numberA = 3;                     //создали переменную целое число -int. 
+int numberB = 5;
+                                    // можно через любую переменную resolt = numberA+numberB
+                                    //  Console.WriteLine (resolt);
+Console.WriteLine(numberA+numberB);
